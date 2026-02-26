05b472a84d007f0da1ef8a3167c009e6021d9e8f
BHAVYA\bhavy <dev.pbhavya@gmail.com>
2026-02-27 00:20:42 +0530

    config

--- client/sb.js
+++ client/sb.js
@@ -1,5 +1,8 @@
 const { createClient } = require("@supabase/supabase-js");
-require("dotenv").config();
+
+if (!process.env.SUPABASE_URL || !process.env.SUPABASE_SERVICE_ROLE_KEY) {
+  throw new Error("Supabase ENV not found. GitHub secrets not injected.");
+}
 
 const supabase = createClient(
   process.env.SUPABASE_URL,
